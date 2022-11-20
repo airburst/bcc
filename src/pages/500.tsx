3153b971a4db5769eb871b153b84291715b688a7
Mark Fairhurst <mark@fairhursts.net>
2022-11-20 16:39:11 +0000

    feat: Add 404 and 500 pages

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,7 +1,5 @@
 import Head from "next/head";
-import Image from "next/future/image";
 import { BackButton } from "../components";
-import NoConnection from "../../public/static/images/no-connection.svg";
 
 const Offline = () => (
   <>
@@ -12,17 +10,14 @@ const Offline = () => (
 
     <div className=" text-neutral-800">
       <div className="flex h-64 items-center justify-center">
-        <Image
-          className="h-32 w-32"
-          src={NoConnection}
-          alt="No internet connection Logo"
-        />
+        <i className="fa-solid fa-circle-exclamation text-8xl text-red-500" />
       </div>
       <div className="flex items-center p-4 text-center text-2xl text-neutral-700">
-        You don&apos;t have an Internet connection right now.
+        Sorry - we can&apos;t get this information right now.
       </div>
       <div className="flex items-center p-4 text-center text-2xl text-neutral-700">
-        You can only view rides that you have already visited.
+        If you don&apos;t have an Internet connection, you can only view rides
+        that you have already visited.
       </div>
       <div className="flex items-center justify-center p-4  text-neutral-700">
         <BackButton />
