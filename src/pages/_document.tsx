1fb4d3e4e2498583b0e2b36e0aca311708884622
Mark Fairhurst <mark@fairhursts.net>
2022-11-20 14:50:57 +0000

    feat: Add iOS spashscreens

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,6 +42,46 @@ const Document = () => (
         href="https://fonts.googleapis.com/css2?family=Prompt:wght@400;500;700&display=swap"
         rel="stylesheet"
       />
+      <link
+        href="/static/splashscreens/iphone5_splash.png"
+        media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
+      />
+      <link
+        href="/static/splashscreens/iphone6_splash.png"
+        media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+      />
+      <link
+        href="/static/splashscreens/iphoneplus_splash.png"
+        media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
+      />
+      <link
+        href="/static/splashscreens/iphonex_splash.png"
+        media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+      />
+      <link
+        href="/static/splashscreens/iphonexr_splash.png"
+        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+      />
+      <link
+        href="/static/splashscreens/iphonexsmax_splash.png"
+        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+      />
+      <link
+        href="/static/splashscreens/ipad_splash.png"
+        media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+      />
+      <link
+        href="/static/splashscreens/ipadpro1_splash.png"
+        media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
+      />
+      <link
+        href="/static/splashscreens/ipadpro3_splash.png"
+        media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
+      />
+      <link
+        href="/static/splashscreens/ipadpro2_splash.png"
+        media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
+      />
       <meta
         name="google-site-verification"
         content="Mo-wRmOnufCVny2ZCkZG6iEZhuO0GMB5jLGUJJ6Ne_0"
