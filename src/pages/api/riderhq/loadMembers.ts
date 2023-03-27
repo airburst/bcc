976a1d19abdaa8de5089641902de596934e290b1
Mark Fairhurst <mark@fairhursts.net>
2023-03-27 22:19:25 +0100

    feat: Change truncate to deleteMany

--- src/pages/api/riderhq/loadMembers.ts
+++ src/pages/api/riderhq/loadMembers.ts
@@ -3,7 +3,8 @@ import { Member } from "./convertMembers";
 
 export const loadMembers = async (members: Member[]) => {
   // Clean existing table
-  await prisma.$queryRaw`TRUNCATE Membership;`;
+  await prisma.membership.deleteMany();
+
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   const result = await prisma.membership.createMany({ data: members });
