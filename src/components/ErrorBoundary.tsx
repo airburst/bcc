91fbd252de2880ce32ea9c9853081d72925058d8
Mark Fairhurst <mark@fairhursts.net>
2023-05-09 16:02:18 +0100

    style: Update error page

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 import React, { Component, ErrorInfo, ReactNode } from "react";
+import { CircleExclamationIcon } from "./Icon";
 
 interface Props {
   children?: ReactNode;
@@ -28,9 +29,17 @@ export class ErrorBoundary extends Component<Props, State> {
     // eslint-disable-next-line react/destructuring-assignment
     if (this.state.hasError) {
       return (
-        <h1 className="text-2xl pt-32 text-center">
-          Sorry; the app is having problems. Please try again later.
-        </h1>
+        <div className="flex flex-col text-neutral-800 justify-center">
+          <div className="flex h-64 items-center justify-center">
+            <CircleExclamationIcon className="fill-red-500 w-24 h-24" />
+          </div>
+          <div className="flex items-center p-4 text-center text-2xl text-neutral-700">
+            Sorry.. the app is experiencing problems
+          </div>
+          <div className="flex items-center p-4 justify-center text-2xl text-neutral-700">
+            Please try again later
+          </div>
+        </div>
       );
     }
 
