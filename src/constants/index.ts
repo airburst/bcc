bfcbc48b0cd0c0b7dcdcafe5ac0696ecd8e2a8a5
Mark Fairhurst <mark@fairhursts.net>
2023-08-12 14:27:59 +0100

    feat: Move all repeating rides to new model Change cron apis

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,10 +1,5 @@
 export * from "./days";
 export * from "./preferences";
-export * from "./rides/friday";
-export * from "./rides/sundayRides";
-export * from "./rides/tuesday";
-export * from "./rides/wednesday";
-export * from "./rides/wednesday-hills";
 
 // [5 .. 50] rider limit bounds
 export const RIDER_LIMIT_OPTIONS: number[] = Array.from(Array(45).keys()).map(
