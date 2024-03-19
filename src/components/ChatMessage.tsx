d092226d0683c54ded2306a10d3004e9874fdb02
Mark Fairhurst <mark.fairhurst@simplybusiness.co.uk>
2024-03-19 16:16:52 +0000

    fix: Dont show avatar for anon

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -1,31 +1,19 @@
 /* eslint-disable @next/next/no-img-element */
 import { RideNote } from "src/types";
 
-const initials = (name: string): string => {
-  if (!name) return "XX";
-
-  const parts = name.split(" ");
-
-  return parts.length > 1
-    ? `${parts[0]?.[0]}${parts[1]?.[0]}`.toUpperCase()
-    : name.slice(0, 2).toUpperCase();
-};
-
 export const ChatMessage: React.FC<RideNote> = ({
   name,
   rideNotes,
   image,
 }: RideNote) => (
   <div className="chat chat-start pl-2">
-    <div className="chat-image avatar placeholder">
-      <div className="bg-neutral text-neutral-content rounded-full w-10">
-        {image ? (
+    {image && (
+      <div className="chat-image avatar placeholder">
+        <div className="bg-neutral text-neutral-content rounded-full w-10">
           <img alt="Tailwind CSS chat bubble component" src={image} />
-        ) : (
-          <span className="text-base">{initials(name)}</span>
-        )}
+        </div>
       </div>
-    </div>
+    )}
     <div className="chat-header">{name}</div>
     <div className="chat-bubble">{rideNotes}</div>
   </div>
